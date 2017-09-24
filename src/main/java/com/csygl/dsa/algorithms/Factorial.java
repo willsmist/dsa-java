786f3b5b3dedbbeebf40e343d88b4d954bbb4a96
Guo-Long Yang <cloudskyygl@gmail.com>
2017-09-24 19:49:00 +0800

    modify Factorial and Fibonacci

--- src/main/java/com/csygl/dsa/algorithms/Factorial.java
+++ src/main/java/com/csygl/dsa/algorithms/Factorial.java
@@ -7,13 +7,30 @@ public class Factorial {
 
     /**
      * 递归计算阶乘
-     * @param n 阶乘参数
+     *
+     * @param n 正整数
      * @return 阶乘的结果
      */
-    public long fact(int n) {
+    public long recurFact(int n) {
         if (n <= 1) {
             return 1L;
         }
-        return n * fact(n - 1);
+        return n * recurFact(n - 1);
+    }
+
+    /**
+     * 迭代计算阶乘
+     * @param n 正整数
+     * @return 阶乘的结果
+     */
+    public long iterFact(int n) {
+        if (n <= 1) {
+            return 1L;
+        }
+        long r = 1L;
+        for (int i = n; i > 0; i--) {
+            r *= i;
+        }
+        return r;
     }
 }
--- src/main/java/com/csygl/dsa/algorithms/Fibonacci.java
+++ src/main/java/com/csygl/dsa/algorithms/Fibonacci.java
@@ -7,6 +7,7 @@ public class Fibonacci {
 
     /**
      * 二分递归计算斐波那契数
+     *
      * @param n 整数
      * @return 返回斐波那契数列中第 n 个数
      */
@@ -18,5 +19,53 @@ public class Fibonacci {
             return 1;
         }
         return binaryFib(n - 1) + binaryFib(n - 2);
+    }
+
+    private int f;//当前 Fibonacci 数
+    private int p;//当前 Fibonacci 数的上一个数
+
+    /**
+     * 线性递归计算斐波那契数
+     *
+     * @param n 整数
+     * @return 返回斐波那契数列中第 n 个数
+     */
+    public int linearFib(int n) {
+        linearFib_recur(n);
+        return f;
+    }
+
+    private void linearFib_recur(int n) {
+        if (n <= 1) {
+            f = n;
+            p = 0;
+            return;
+        }
+        linearFib_recur(n - 1);
+        int t = f;
+        f = f + p;
+        p = t;
+    }
+
+    /**
+     * 迭代计算斐波那契数
+     * @param n 整数
+     * @return 返回斐波那契数列中第 n 个数
+     */
+    public int iterFib(int n) {
+        if (n == 0) {
+            return 0;
+        }
+        if (n == 1) {
+            return 1;
+        }
+        int a = 0;
+        int b = 1;
+        for (int i = 2; i <= n; i++) {
+            int t = a;
+            a = b;
+            b += t;
+        }
+        return b;
     }
 }
