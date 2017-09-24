cf2c3a1928532bb47f9f96b9639be2e559327955
Guo-Long Yang <cloudskyygl@gmail.com>
2017-09-25 01:11:17 +0800

    modify Power.java and PowerTest.java

--- src/test/java/com/csygl/dsa/algorithms/PowerTest.java
+++ src/test/java/com/csygl/dsa/algorithms/PowerTest.java
@@ -5,11 +5,23 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class PowerTest extends TestBase {
+
     @Test
     public void powerBrute() {
         Power p = new Power();
         assertEquals(125,p.powerBrute(5,3));
-        System.out.println(p.powerBrute(2,3));
+        System.out.println(p.powerBrute(3,20));
     }
 
+    @Test
+    public void powerRecur() {
+        Power p = new Power();
+        System.out.println(p.powerRecur(3, 20));
+    }
+
+    @Test
+    public void powerRecurFast(){
+        Power p = new Power();
+        System.out.println(p.powerRecurFast(3, 20));
+    }
 }
