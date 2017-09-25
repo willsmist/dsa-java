0f10c7bf828c0bbcdea8ada4efdb64ab1b406021
Guo-Long Yang <cloudskyygl@gmail.com>
2017-09-26 04:18:40 +0800

    reformat code

--- src/test/java/com/csygl/dsa/algorithms/ArrayUtilTest.java
+++ src/test/java/com/csygl/dsa/algorithms/ArrayUtilTest.java
@@ -7,7 +7,7 @@ import java.util.Arrays;
 
 public class ArrayUtilTest {
     @Test
-    public void reverse() throws Exception {
+    public void reverse() {
         Integer[] s = SequenceUtil.randomSeqWithNoDups(50,8);
         System.out.println(Arrays.toString(s));
         ArrayUtil.reverse(s);
