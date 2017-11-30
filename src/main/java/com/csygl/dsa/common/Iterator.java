22ba4eef6f14ce2ccb2984b383a43a9a7b84feb3
Will Yang <cloudskyygl@gmail.com>
2017-11-30 12:27:46 +0800

    add interface Iterator and its simple implementations

---
+++ src/main/java/com/csygl/dsa/common/Iterator.java
@@ -0,0 +1,7 @@
+package com.csygl.dsa.common;
+
+/**
+ * 迭代器接口
+ */
+public interface Iterator<E> {
+}
---
+++ src/main/java/com/csygl/dsa/list/PositionIterator.java
@@ -0,0 +1,6 @@
+package com.csygl.dsa.list;
+
+import com.csygl.dsa.common.Iterator;
+
+public class PositionIterator<E> implements Iterator<E> {
+}
